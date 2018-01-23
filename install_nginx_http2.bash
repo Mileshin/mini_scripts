fb2a24041d11c4e1646daa098bd5999ddb4114c7
Andrey Mileshin <andrej.mileshin@ya.ru>
2018-01-23 14:39:31 +0300

    Added install_nginx_http2.bash

---
+++ install_nginx_http2.bash
@@ -0,0 +1,11 @@
+apt-get install libpcre3 libpcre3-dev libpcrecpp0 libssl-dev zlib1g-dev
+cd /opt
+wget http://nginx.org/download/nginx-1.13.8.tar.gz
+wget https://www.openssl.org/source/openssl-1.0.2n.tar.gz
+tar xf nginx-1.13.8.tar.gz
+tar xf openssl-1.0.2n.tar.gz
+cd nginx-1.13.8/
+./configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --modules-path=/usr/lib/nginx/modules --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --http-client-body-temp-path=/var/cache/nginx/client_temp --http-proxy-temp-path=/var/cache/nginx/proxy_temp --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp --http-scgi-temp-path=/var/cache/nginx/scgi_temp --user=nginx --group=nginx --with-compat --with-file-aio --with-threads --with-http_addition_module --with-http_auth_request_module --with-http_dav_module --with-http_flv_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_mp4_module --with-http_random_index_module --with-http_realip_module --with-http_secure_link_module --with-http_slice_module --with-http_ssl_module --with-http_stub_status_module --with-http_sub_module --with-http_v2_module --with-mail --with-mail_ssl_module --with-stream --with-stream_realip_module --with-stream_ssl_module --with-stream_ssl_preread_module --with-openssl=/opt/openssl-1.0.2n
+make
+make install
+service nginx restart
