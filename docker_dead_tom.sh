e05ef1e78530094b81cfa175105ea19c9d5d027c
Mileshin <andrej.mileshin@ya.ru>
2018-03-24 13:23:25 +0300

    added docker cleanup script

---
+++ docker_dead_tom.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+find '/var/lib/docker/volumes/' -mindepth 1 -maxdepth 1 -type d | grep -vFf <(
+  docker ps -aq | xargs docker inspect | jq -r '.[]|.Mounts|.[]|.Name|select(.)'
+)
