3087f86e4f003cbf6dceede04309c0aa7bdf556d
Mileshin <andrej.mileshin@ya.ru>
2018-05-14 15:18:53 +0300

    update zimbra_update_mailbox_quotas.sh

--- zimbra_update_mailbox_quotas.sh
+++ zimbra_update_mailbox_quotas.sh
@@ -5,5 +5,6 @@ SERVER=zimbra.isu.ifmo.ru
 
 for P in `zmprov gqu @$SERVER |  awk -v Q=$QUOTASIZE '{ if (($2 < Q) && ($2 != 0)) { print  $1 } }'`; do 
 	echo change $P
-	zmprov ma $P zimbraMailQuota $QUOTASIZE
+	zmprov ma $P zimbraMailQuota "" #reset to Class Of Service
+	#zmprov ma $P zimbraMailQuota $QUOTASIZE # set new quota
   done
